32e2794904055ba9b7d336a48f6767d4a7402d9d
Nandhini M <nandhinimm4706@gmail.com>
2026-01-30 22:43:35 +0530

    book class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.*;
+import Model.Book;
 public class Main{
     public static void main(String[]args){
+
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("-------------------Library Management System-----------------------");
+        System.out.println("1.Admin Login");
 
     }
 }
--- src/Model/Book.java
+++ src/Model/Book.java
@@ -1,4 +1,28 @@
 package Model;
 
 public class Book {
+    private int bookId;
+    private String bookName;
+    private String bookAuthor;
+    private String bookDescription;
+
+    public Book(String bookAuthor, int bookId, String bookDescription, String bookName) {
+        this.bookAuthor = bookAuthor;
+        this.bookId=bookId;
+        this.bookDescription=bookDescription;
+        this.bookName=bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+    public String getBookName(){
+        return bookName;
+    }
+    public String getBookDescription(){
+        return bookDescription;
+    }
+    public int getBookId(){
+        return bookId;
+    }
 }
