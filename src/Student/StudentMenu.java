71b78fb6dc19cb1fc933e24803fd28b4c02bcc7d
Nandhini M <nandhinimm4706@gmail.com>
2026-02-02 23:14:55 +0530

    classes

--- src/Student/StudentMenu.java
+++ src/Student/StudentMenu.java
@@ -1,4 +1,25 @@
 package Student;
 
 public class StudentMenu {
+    private String studentId;
+    private String studentName;
+    private String studentDept;
+
+    public StudentMenu(String studentDept,String studentId, String studentName) {
+        this.studentDept = studentDept;
+        this.studentId=studentId;
+        this.studentName=studentName;
+    }
+
+    public String getStudentDept() {
+        return studentDept;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
 }
